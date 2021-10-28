a078b34f782705f295df33b722f24129b3661ce8
James <sparky0226+1@gmail.com>
2021-10-28 16:43:23 +0100

    Added files

---
+++ src/lib/_types.ts
@@ -0,0 +1,11 @@
+export type TransactionFieldsT = Readonly<{
+    from: string;
+    to: string;
+    amount: string;
+    rri: string;
+}>
+
+export type NetworkT = Readonly<{
+    name: string;
+    url: string;
+}>
---
+++ src/lib/cerbymask.ts
@@ -0,0 +1,6 @@
+import { TransactionFieldsT } from "./_types";
+
+export function createNewTransaction(fields: TransactionFieldsT) {
+    console.log("createNewTransaction called")
+    console.log(fields)
+}
