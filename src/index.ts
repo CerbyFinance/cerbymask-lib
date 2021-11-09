95337feb8dcf583c0a53496dcf51ffa2b9e53c3e
James <sparky0226+1@gmail.com>
2021-11-09 23:05:57 +0000

    Update lib

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,58 @@
-export * from './lib/cerbymask'
-export * from './lib/_types'
+
+declare module CerbymaskLib {
+    function submitTransaction()
+}
+
+export interface RequestT {
+    title: string;
+    data: any;
+}
+
+export interface TransactionFieldsT {
+    _id?: string;
+    from: string;
+    to: string;
+    amount: string;
+    rri: string;
+    token: number;
+}
+
+export interface NetworkT {
+    name: string;
+    url: string;
+}
+
+export interface SignedTransactionT {
+    blob: string;
+    publicKeyOfSigner: string;
+    signatureDER: string;
+}
+
+export interface ValidatorT {
+    address: string;
+    infoURL: string;
+    isExternalStakeAccepted: boolean;
+    name: string;
+    ownerAddress: string;
+    ownerDelegation: string;
+    proposalsCompleted: string;
+    proposalsMissed: string;
+    registered: boolean;
+    totalDelegatedStake: string;
+    uptimePercentage: string;
+    validatorFee: string;
+}
+
+export interface PromotedValidatorT extends ValidatorT {
+
+}
+
+export interface StakeT {
+    from: string;
+    validator: string;
+    amount: string;
+}
+
+export const lib = CerbymaskLib
+
+export default lib
--- src/lib/cerbymask.ts
+++
@@ -1,6 +0,0 @@
-import { TransactionFieldsT } from "./_types";
-
-export function createNewTransaction(fields: TransactionFieldsT) {
-    console.log("createNewTransaction called")
-    console.log(fields)
-}
